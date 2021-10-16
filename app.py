067c378ab7263a2629ec72ef06dcfca6c8d26cfe
dvini0208 <92649519+dvini0208@users.noreply.github.com>
2021-10-17 01:37:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "manbearpig_MUDMAN888"
+
+
+@app.route("/hello")
+def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", great to see you here.. Development is in progress")
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
